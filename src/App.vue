0f52df2fa9aa181d74e4155ff28d59303eb12786
niepsuje <jakubniepsujj@gmail.com>
2023-10-11 14:24:31 +0200

    initiating the project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  // import {RouterView} from "vue-router"
+  // import QuizesView from "./views/QuizesView.vue"
+
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text"/>
+    </header>
+    <div class="options-container">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    max-width: 1000px;
+    margin: 0 auto
+  }
+
+  .header {
+    margin-bottom: 10px;
+    margin-top: 25px;
+    display: flex;
+    align-items:center
+  }
+</style>
