199b66be2c3b1a7ce741f2f5b88e2a4015deb39f
niepsuje <jakubniepsujj@gmail.com>
2023-10-12 11:43:58 +0200

    Styled the front application.

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,16 @@
   <div class="container">
     <header>
       <h1>Quizes</h1>
-      <input type="text"/>
+      <input type="text" placeholder="Search..."/>
     </header>
     <div class="options-container">
-
+      <div class="card">
+        <img src="https://img.freepik.com/free-vector/counting-number-0-9-math-symbols_1308-104139.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696982400&semt=sph" alt="">
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 questions</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,4 +34,50 @@
     display: flex;
     align-items:center
   }
+
+  header h1 {
+    font-weight: bold;
+    margin-right: 30px;
+  }
+
+  header input {
+    border: 4mm ridge rgba(211, 220, 50, .6);
+    background-color: rgb(130, 130, 140, 0.2);
+    padding: 8px;
+    border-radius: 8px;
+  }
+
+  .options-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 20px;
+  }
+
+  /* CARD */
+
+  .card {
+    width: 340px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgb(123, 161, 9);
+    margin-bottom: 300px;
+    margin-right: 25px;
+    cursor: pointer;
+  }
+
+  .card img {
+    width: 100%;
+    height: 180px;
+    margin: 1px;
+  }
+
+  .card .card-text {
+    padding: 0 50px;
+  }
+
+  .card .card-text h2 {
+    font-weight: bold;
+    font-family: "Gill Sans", sans-serif;
+  }
+
 </style>
