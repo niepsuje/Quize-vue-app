fc641b4b1f84d6ce21bbdf3c2c76b56474c059a1
niepsuje <jakubniepsujj@gmail.com>
2023-10-14 11:41:52 +0200

    Formatting and isolating code into smaller parts in separate files.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import q from "./data/quizes.json"
   import {ref, watch} from "vue"
+  import Card from "./components/Card.vue"
 
   const quizes = ref(q)
   const search = ref("")
@@ -18,13 +19,14 @@
       <input v-model.trim="search" type="text" placeholder="Search..."/>
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="">
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length }} questions</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -34,7 +36,6 @@
     max-width: 1000px;
     margin: 0 auto
   }
-
   .header {
     margin-bottom: 10px;
     margin-top: 25px;
@@ -61,30 +62,5 @@
   }
 
   /* CARD */
-
-  .card {
-    width: 340px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgb(123, 161, 9);
-    margin-bottom: 25px;
-    margin-right: 25px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 180px;
-    margin: 1px;
-  }
-
-  .card .card-text {
-    padding: 0 50px;
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
-    font-family: "Gill Sans", sans-serif;
-  }
 
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,45 @@
+<script setup>
+    import {defineProps} from "vue"
+
+    const { quiz } = defineProps(['quiz'])
+
+    console.log(quiz)
+</script>
+
+<template>
+    <div class="card">
+        <img :src="quiz.img" alt="">
+        <div class="card-text">
+            <h2>{{ quiz.name }}</h2>
+            <p>{{ quiz.questions.length }} questions</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+  .card {
+    width: 340px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgb(123, 161, 9);
+    margin-bottom: 25px;
+    margin-right: 25px;
+    cursor: pointer;
+  }
+
+  .card img {
+    width: 100%;
+    height: 180px;
+    margin: 1px;
+  }
+
+  .card .card-text {
+    padding: 0 50px;
+  }
+
+  .card .card-text h2 {
+    font-weight: bold;
+    font-family: "Gill Sans", sans-serif;
+  }
+
+</style>
